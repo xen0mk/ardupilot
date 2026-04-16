ea55f4ea9f959119d9e5733011c774267cb08b15
Xen0mk <42239691+xen0mk@users.noreply.github.com>
2026-04-16 02:42:15 +0200

    Update AP_Motors6DOF.cpp
    Added SUB_FRAME_CUSTOM configuration for CPS 5 drone

--- libraries/AP_Motors/AP_Motors6DOF.cpp
+++ libraries/AP_Motors/AP_Motors6DOF.cpp
@@ -176,8 +176,13 @@ void AP_Motors6DOF::setup_motors(motor_frame_class frame_class, motor_frame_type
         break;
 
     case SUB_FRAME_CUSTOM:
-        // Put your custom motor setup here
-        //break;
+        _frame_class_string = "CUSTOM";
+        add_motor_raw_6dof(AP_MOTORS_MOT_1,     0,              0,              -1.0f,          0,                  1.0f,               0,              1); // Motor R
+        add_motor_raw_6dof(AP_MOTORS_MOT_2,     0,              0,              -1.0f,          0,                  -1.0f,              0,              2); // Motor L
+        add_motor_raw_6dof(AP_MOTORS_MOT_3,     -1.0f,          1.0f,           0,              1.0f,               0,                  0,              3); // Motor B
+        add_motor_raw_6dof(AP_MOTORS_MOT_4,     1.0f,           1.0f,           0,              1.0f,               0,                  0,              4); // Motor A
+        add_motor_raw_6dof(AP_MOTORS_MOT_5,     0,              -1.0f,          0,              1.0f,               0,                  0,              5); // Motor C
+        break;
 
     case SUB_FRAME_SIMPLEROV_3:
         _frame_class_string = "SIMPLEROV_3";
